634fe20b0252ac2f39277b0a590f9c15c7dd330a
nickmagruder <nick.magruder@outlook.com>
2021-05-25 23:14:07 -0700

    monster in progress

--- src/App.js
+++ src/App.js
@@ -2,30 +2,29 @@ import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      string: 'Hello Nick'
+      monsters: [ ]
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
   }
 
   render() {
     return (
 
       <div className="App">
-        <header className="App-header">
 
-          <img src={logo} className="App-logo" alt="logo" />
+        { this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>) }
 
-          <h1>
-            {this.state.string}
-          </h1>
-
-          <button onClick={() => this.setState({ string: 'Hello new human' })}>Change Greeting</button>
-
-        </header>
       </div>
     );
   }
